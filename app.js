b60131c0798f47130d99d18f883eb9b5abe7a815
ArindamBharati <arindambharati@gmail.com>
2021-09-13 12:23:16 +0530

    refactoring i/p o/p console

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 var btnSpeak = document.querySelector("#btn-speak");
 var txtInput = document.querySelector("#txtarea-en");
 
-btnSpeak.addEventListener("click",function clickEventHandler(){
-    console.log("clicked");
-    console.log("input",txtInput.value);
-})
+function clickHandler() {
+        console.log("clicked");
+        console.log("input", txtInput.value);
+}
+
+btnSpeak.addEventListener("click",clickHandler);
+
