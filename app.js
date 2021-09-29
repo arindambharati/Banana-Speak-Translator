a5cf7ee0f79380aa7b95edbf4c17b29fd9a12f8c
ArindamBharati <arindambharati@gmail.com>
2021-09-30 00:51:30 +0530

    Testing data from server

--- app.js
+++ app.js
@@ -2,8 +2,24 @@ var btnSpeak = document.querySelector("#btn-speak");
 var txtInput = document.querySelector("#txtarea-en");
 var txtOutput = document.querySelector("#output");
 
+var serverURL="https://lessonfourapi.ArindamBharati.repl.co/translate/yoda.json";
+// https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json?text=I am Arindam"
+
+
+function getTranslationURL(text)
+{
+        return serverURL + "?" + "text=" + text;
+}
+
 function clickHandler() {
-        txtOutput.innerText="xasdihoid baaannna uggaag saaa"+ txtInput.value;
+        var inputText=txtInput.value;
+
+        fetch(getTranslationURL(inputText))
+        .then(response=>response.json())
+        .then(json => {
+                var translation=json.contents.translated;
+                txtOutput.innerText=translation;
+        })
 }
 
 btnSpeak.addEventListener("click",clickHandler);
