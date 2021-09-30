f13f55b77b5083fc65d2aaac8f51c529c1ec4a11
ArindamBharati <arindambharati@gmail.com>
2021-09-30 20:00:07 +0530

    Design added

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var txtOutput = document.querySelector("#output");
 var serverURL="https://api.funtranslations.com/translate/minion.json";
 
 
+
 function getTranslationURL(text)
 {
         return serverURL + "?" + "text=" + text;
@@ -24,7 +25,6 @@ function clickHandler() {
                 var translation=json.contents.translated;
                 txtOutput.innerText=translation;
         })
-
         .catch(errorHandler)
 }
 
