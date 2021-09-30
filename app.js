eeb8b94d23bed5099da219e2bf044600717c1ba6
ArindamBharati <arindambharati@gmail.com>
2021-09-30 20:01:16 +0530

    blank space edit

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var txtOutput = document.querySelector("#output");
 var serverURL="https://api.funtranslations.com/translate/minion.json";
 
 
-
 function getTranslationURL(text)
 {
         return serverURL + "?" + "text=" + text;
